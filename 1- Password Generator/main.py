222908d662a1803de37103730913831889e14f88
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-18 16:49:53 +0200

    Update main.py
    Została utworzona funkcja zadająca pytanie o długość hasła

--- 1- Password Generator/main.py
+++ 1- Password Generator/main.py
@@ -1,6 +1,9 @@
 import string
 from random import randint
 
+MIN_PASSWORD_LENGTH = 8
+MAX_PASSWORD_LENGTH = 40
+
 
 def password_generator(n):
     elements = list(string.ascii_letters + string.digits + string.punctuation)
@@ -10,11 +13,14 @@ def password_generator(n):
     return password
 
 
-n = 0
+def ask_about_password_length(password_length = 0):
+    while password_length < MIN_PASSWORD_LENGTH or password_length > MAX_PASSWORD_LENGTH:
+        password_length = int(input("Podaj długość hasła: "))
+        if password_length < MIN_PASSWORD_LENGTH or password_length > MAX_PASSWORD_LENGTH:
+            print("Hasło powinno zawierać od 8 do 40 znaków, spróbuj ponownie")
 
-while n < 8 or n > 40:
-    n = int(input("Podaj długość hasła: "))
-    if n < 8 or n > 40:
-        print("Hasło powinno zawierać od 8 do 40 znaków, spróbuj ponownie")
+    return password_length
 
-print(password_generator(n))
+
+password_length = ask_about_password_length()
+print(password_generator(password_length))
