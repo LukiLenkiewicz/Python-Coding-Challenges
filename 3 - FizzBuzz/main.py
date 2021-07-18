18bbe69191b4ab48578da11ee4a8606ab561cc48
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-18 20:18:22 +0200

    Update main.py

--- 3 - FizzBuzz/main.py
+++ 3 - FizzBuzz/main.py
@@ -1,22 +1,16 @@
 START = 1
-STOP = 101
+STOP = 100
 
 def fizzbuzz():
-    for i in range(START, STOP):
+    for i in range(START, STOP + 1):
         ans = ''
         if i % 3 == 0:
             ans += 'Fizz'
         if i % 5 == 0:
             ans += 'Buzz'
-
-        print_solution(ans, i)
-
-
-def print_solution(ans, i):
-    if len(ans) > 0:
-        print(ans)
-    else:
-        print(i)
-
-
+        if len(ans) > 0:
+            print(ans)
+        else:
+            print(i)
+            
 fizzbuzz()
