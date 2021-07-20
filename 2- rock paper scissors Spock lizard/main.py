37fabe46c79dfc4edcdfcb95e1f8d717ef4aca7d
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-20 22:49:40 +0200

    Update main.py

--- 2- rock paper scissors Spock lizard/main.py
+++ 2- rock paper scissors Spock lizard/main.py
@@ -22,11 +22,10 @@ def gameplay():
     computer_points = 0
     game_on = True
     while game_on:
-        player_move = input("Paper\nRock\nScissors\nSpock\nLizard\nWybierz jedną z powyższych figur: ")
+        player_move = input('Paper\nRock\nScissors\nSpock\nLizard\nWybierz jedną z powyższych figur lub wpisz "STOP" aby zakończyć program: ')
         computer_move = computer_moves[randint(1, 5)]
         if player_move == "STOP": game_on = False
         if game_on:
-            """Do tych ifów dorobić funkcje"""
             if computer_move in beats[player_move]:
                 player_points += 1
             elif player_move in beats[computer_move]:
