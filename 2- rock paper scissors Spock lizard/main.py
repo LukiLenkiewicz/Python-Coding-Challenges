23b8e6b1c18ae67c79c4733276bee1593cf9e716
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-22 09:45:02 +0200

    Update main.py

--- 2- rock paper scissors Spock lizard/main.py
+++ 2- rock paper scissors Spock lizard/main.py
@@ -1,7 +1,5 @@
 from random import choice
 
-MOVES = ["Paper", "Rock", "Scissors", "Spock", "Lizard"]
-
 BEATS = {
     "Paper": ["Rock", "Spock"],
     "Rock": ["Scissors", "Lizard"],
@@ -10,12 +8,16 @@ BEATS = {
     "Lizard": ["Paper", "Spock"]
 }
 
+MOVES = list(BEATS.keys())
+
+USER_EXIT_COMMAND = "Stop"
+
 
 def player_turn():
     player_move = input()
     player_move = player_move.capitalize()
 
-    while player_move not in MOVES and player_move != "Stop":
+    while player_move not in MOVES and player_move != USER_EXIT_COMMAND:
         print("Zły ruch, spróbuj ponownie: ")
         player_move = input()
         player_move = player_move.capitalize()
@@ -23,29 +25,35 @@ def player_turn():
     return player_move
 
 
-def gameplay():
+def compare_moves(player_move, computer_move, player_points, computer_points):
+    if computer_move in BEATS[player_move]:
+        print(f"{player_move} beats {computer_move}")
+        return player_points + 1, computer_points
 
+    elif player_move in BEATS[computer_move]:
+        print(f"{computer_move} beats {player_move}")
+        return player_points, computer_points + 1
+
+    else:
+        print("Remis")
+        return player_points, computer_points
+
+
+def gameplay():
     player_points = 0
     computer_points = 0
 
     while True:
         for move in MOVES:
             print(move, end='\t')
-        print('\nWybierz jedną z powyższych figur lub wpisz "Stop" aby zakończyć: ')
+        print(f'\nWybierz jedną z powyższych figur lub wpisz "{USER_EXIT_COMMAND}" aby zakończyć: ')
 
         player_move = player_turn()
         computer_move = choice(MOVES)
-        if player_move == "Stop":
+        if player_move == USER_EXIT_COMMAND:
             break
 
-        if computer_move in BEATS[player_move]:
-            player_points += 1
-            print(f"{player_move} beats {computer_move}")
-        elif player_move in BEATS[computer_move]:
-            computer_points += 1
-            print(f"{computer_move} beats {player_move}")
-        else:
-            print("Remis")
+        player_points, computer_points = compare_moves(player_move, computer_move, player_points, computer_points)
 
         print(f"Punkty gracza: {player_points}\nPunkty komputera: {computer_points}\n")
 
