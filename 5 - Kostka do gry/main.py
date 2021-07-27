d4cb9c8e1a642da017b715c738bba7ea3a5f8ec6
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-27 21:53:05 +0200

    Update main.py

--- 5 - Kostka do gry/main.py
+++ 5 - Kostka do gry/main.py
@@ -3,37 +3,53 @@ from random import randint
 MIN_NUM_OF_DOTS = 1
 MAX_NUM_OF_DOTS = 6
 
-CONTINUE_THE_GAME = ['y', 'yes', 't', 'tak']
-STOP_THE_GAME = ['n', 'no', 'nie']
-
 MAX_NUM_OF_DICES = 4
 MIN_NUM_OF_DICES = 1
 
+CONTINUE_THE_GAME = ['y', 'yes', 't', 'tak']
+STOP_THE_GAME = ['n', 'no', 'nie']
+
 
 def choose_number_of_dices():
-    number_of_throws = int(input("Masz 4 kości, iloma chcesz rzucić? "))
+    while True:
+        number_of_throws = int(input("Masz 4 kości, iloma chcesz rzucić? "))
+        if number_of_throws <= MAX_NUM_OF_DICES and number_of_throws >= MIN_NUM_OF_DICES:
+            return number_of_throws
+        else:
+            print("Nieodpowiednia liczba kości. Spróbuj ponownie.")
 
-    while number_of_throws > MAX_NUM_OF_DICES or number_of_throws < MIN_NUM_OF_DICES:
-        number_of_throws = int(input("Nieodpowiednia liczba kości. Spróbuj ponownie: "))
 
-    return number_of_throws
+def print_message():
+    print("Czy chcesz grać dalej? ")
+    print("Wpisz: ", end='')
+    for word in CONTINUE_THE_GAME:
+        print(f"{word}, ", end='')
+    print("żeby kontynuować")
+    print("Lub ", end='')
+    for word in STOP_THE_GAME:
+        print(f"{word}, ", end='')
+    print("żeby zakończyć")
+    return input()
+
+
+def continue_the_game():
+    while True:
+        answer = print_message()
+        if answer in CONTINUE_THE_GAME:
+            return True
+        elif answer in STOP_THE_GAME:
+            return False
+        else:
+            print("Podano nieprawidłową wartość, spróbuj ponownie.")
 
 
 def throw():
-
     number_of_throws = choose_number_of_dices()
-
-    while True:
+    game_on = True
+    while game_on:
         result = randint(number_of_throws*MIN_NUM_OF_DOTS, number_of_throws*MAX_NUM_OF_DOTS)
         print(f"Uzyskana liczba oczek to: {result}")
-
-        continue_game = input("Czy chcesz grać dalej? ")
-
-        if continue_game in CONTINUE_THE_GAME:
-            continue
-        elif continue_game in STOP_THE_GAME:
-
-            break
+        game_on = continue_the_game()
 
 
 throw()
