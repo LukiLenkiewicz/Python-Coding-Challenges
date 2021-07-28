f1bf88e19f36a214e81e65fb0738941d0788074c
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-28 10:09:33 +0200

    Update main.py

--- 6 - Kalkulator dat/main.py
+++ 6 - Kalkulator dat/main.py
@@ -1,68 +1,52 @@
-import datetime
 import time
 
-REGULAR_MONTHS = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31
-}
-
-REGULAR_MONTHS = list(REGULAR_MONTHS)
 REGULAR_YEAR = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 LEAP_YEAR = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+HOURS_IN_A_DAY = 24
+SECONDS_IN_A_MINUTE = 60
+SECONDS_IN_ONE_HOUR = 3600
 
-def calculate_time(today, not_today):
+
+def input_data():
+    compared_date = input("Podaj datę od której chcesz policzyć czas w formacie rrrr-mm-dd: ")
+    compared_date = compared_date.split('-')
+    return list(map(int, compared_date))
+
+
+def calculate_days(today, not_today):
     days_passed = 0
     for i in range(today[0] - not_today[0]):
         if (not_today[0] + i) % 4 == 0 or (not_today[0] + i) % 400 == 0:
             days_passed += 366
         else:
             days_passed += 365
-
-    if (today[0] + i + 1) % 4 == 0 or (today[0] + i + 1) % 400 == 0:
+    if today[0] % 4 == 0 or today[0] % 400 == 0:
         for month in LEAP_YEAR[:today[1] - 1]:
             days_passed += month
     else:
         for month in REGULAR_YEAR[:today[1] - 1]:
             days_passed += month
 
-    if (not_today[0] + i + 1) % 4 == 0 or (not_today[0] + i + 1) % 400 == 0:
+    if not_today[0] % 4 == 0 or not_today[0] % 400 == 0:
         for month in LEAP_YEAR[:not_today[1] - 1]:
             days_passed -= month
     else:
         for month in REGULAR_YEAR[:not_today[1] - 1]:
             days_passed -= month
-
     days_passed = days_passed + today[2] - not_today[2]
-    # days_passed += today[2]
-    # days_passed -= not_today[2]
     return days_passed
 
 
 def data_calculator():
     todays_time = list(time.localtime())[:6]
-    print(todays_time)
-    compared_date = input("Podaj datę od której chcesz policzyć czas w formacie dd-mm-rrrr: ")
-    compared_date = compared_date.split('-')
-    compared_date.reverse()
-    # compared_hour = input("Podaj godzinę w formacie hh:mm: ")
-    # compared_hour = list(compared_hour.split(':'))
-    # compared_date.append(compared_hour[0])
-    # compared_date.append(compared_hour[1])
-    compared_date = list(map(int, compared_date))
-
-    time_passed = calculate_time(todays_time, compared_date)
-    print(time_passed)
+    compared_date = input_data()
+    time_passed = calculate_days(todays_time, compared_date)
+    print(f"Od wybranej daty minęło {time_passed} dni.")
+    time_passed = time_passed*HOURS_IN_A_DAY + todays_time[3]
+    print(f"Jest to {time_passed} godzin.")
+    time_passed = time_passed*SECONDS_IN_ONE_HOUR + todays_time[4]*SECONDS_IN_A_MINUTE + todays_time[5]
+    print(f"LUB {time_passed} sekund.")
 
 
 data_calculator()
