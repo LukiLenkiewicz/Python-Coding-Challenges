0a8309663f9fce2a35cae24ceedfa970ab0c307d
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-07-30 10:09:56 +0200

    Update main.py

--- 6 - Kalkulator dat/main.py
+++ 6 - Kalkulator dat/main.py
@@ -4,14 +4,26 @@ REGULAR_YEAR = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 LEAP_YEAR = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 HOURS_IN_A_DAY = 24
+MINUTES_IN_ONE_HOUR = 60
 SECONDS_IN_A_MINUTE = 60
-SECONDS_IN_ONE_HOUR = 3600
+
+NR_OF_DATE_PARAMETERS = 3
 
 
 def input_data():
-    compared_date = input("Podaj datę od której chcesz policzyć czas w formacie rrrr-mm-dd: ")
-    compared_date = compared_date.split('-')
-    return list(map(int, compared_date))
+    
+    while True:
+        compared_date = input("Podaj datę od której chcesz policzyć czas w formacie rrrr-mm-dd: ")
+        compared_date = compared_date.split('-')
+        error_message = ''
+        if len(compared_date) != NR_OF_DATE_PARAMETERS:
+            error_message += 'Niepoprawny zapis.\n'
+        elif int(compared_date[2]) > 12 and int(compared_date[2]) < 1:
+            error_message += 'Niepoprawna długość miesięcy.\n'
+        else:
+            return list(map(int, compared_date))
+
+    
 
 
 def days_correction(today, given_day):
@@ -49,8 +61,10 @@ def data_calculator():
     print(f"Od wybranej daty minęło {time_passed} dni.")
     time_passed = time_passed*HOURS_IN_A_DAY + todays_time[3]
     print(f"Jest to {time_passed} godzin.")
-    time_passed = time_passed*SECONDS_IN_ONE_HOUR + todays_time[4]*SECONDS_IN_A_MINUTE + todays_time[5]
-    print(f"LUB {time_passed} sekund.")
+    time_passed = time_passed*MINUTES_IN_ONE_HOUR + todays_time[4]
+    print(f"Lub {time_passed} minut.")
+    time_passed = time_passed*SECONDS_IN_A_MINUTE + todays_time[5]
+    print(f"Lub {time_passed} sekund.")
 
 
 data_calculator()
