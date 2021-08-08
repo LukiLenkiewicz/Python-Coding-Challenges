46a09016d48f93b7f79961c0a18079abe57e3e52
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-08-08 14:06:29 +0200

    Update main.py

--- 7 - ToDoList/main.py
+++ 7 - ToDoList/main.py
@@ -1,13 +1,14 @@
 import json
 import os
 
-JSON_NAME = 'tasks.json'
+JSON_FILE_NAME = 'tasks.json'
+EXIT_COMMAND = 'exit'
 
 
 def todolist():
     tasks = creating_new_file()
     user_command = ''
-    while user_command != 'exit':
+    while user_command != EXIT_COMMAND:
         print_tasks(tasks)
         user_command = input('Podaj komendę użytkownika lub wpisz "help" aby uzyskać pomoc: ')
         tasks = checking_user_input(tasks, user_command)
@@ -15,13 +16,13 @@ def todolist():
 
 
 def creating_new_file():
-    if os.path.exists(JSON_NAME):
-        with open(JSON_NAME, 'r') as file:
+    if os.path.exists(JSON_FILE_NAME):
+        with open(JSON_FILE_NAME, 'r') as file:
             return json.load(file)
     else:
         print('Tworzenie pustej listy zadań')
         tasks = {}
-        with open(JSON_NAME, 'w') as file:
+        with open(JSON_FILE_NAME, 'w') as file:
             json.dump(tasks, file)
         return tasks
 
@@ -31,17 +32,37 @@ def add_task(tasks, task_description):
     while True:
         if str(task_id) not in list(tasks.keys()):
             tasks[str(task_id)] = {'Status': 'NEW', 'Description': task_description[4:]}
+            print(f"Zadanie \"{tasks[str(task_id)]['Description']}\" dodane z powodzeniem.\n")
             return tasks
         else:
             task_id += 1
+
+
+def remove_task(task_id, tasks):
+    if task_id in list(tasks.keys()):
+        del tasks[task_id]
+        print("Zadanie usunięte z powodzeniem\n")
+        return tasks
+    else:
+        print("Nie ma takiego ID")
+        return tasks
+
+
+def resolve_task(task_id, tasks):
+    if task_id in list(tasks.keys()):
+        tasks[task_id]['Status'] = 'DONE'
+        return tasks
+    else:
+        print("Nie ma takiego ID.")
+        return tasks
 
 
 def print_tasks(tasks):
     if len(tasks) == 0:
         print("Lista zadań jest pusta.")
     else:
-        for task_id, j in tasks.items():
-            print(task_id, ' ', j['Status'], ' ', j['Description'])
+        for task_id, task in tasks.items():
+            print(f"{task_id} {task['Status']} {task['Description']}")
 
 
 def splitting_user_input(command):
@@ -50,25 +71,21 @@ def splitting_user_input(command):
 
 
 def checking_user_input(tasks, user_command):
+    ADDING_TASK_COMMAND = 'add'
+    REMOVE_TASK_COMMAND = 'remove'
+    RESOLVE_TASK_COMMAND = 'resolve'
+    PRINT_HELP_COMMAND = 'help'
     command_check, split_user_command = splitting_user_input(user_command)
-    if command_check.lower() == 'add':
+    if command_check.lower() == ADDING_TASK_COMMAND:
         tasks = add_task(tasks, user_command)
-        print("Zadanie dodane z powodzeniem.\n")
-    elif command_check.lower() == 'remove':
-        if split_user_command[1] in list(tasks.keys()):
-            del tasks[split_user_command[1]]
-            print("Zadanie usunięte z powodzeniem\n")
-        else:
-            print("Nie ma takiego ID.")
-    elif command_check.lower() == 'resolve':
-        if split_user_command[1] in list(tasks.keys()):
-            tasks[split_user_command[1]]['Status'] = 'Done'
-        else:
-            print("Nie ma takiego ID.")
-    elif command_check.lower() == 'help':
-        print(f'"add + ID" dodaje zadanie\n"remove + ID" usuwa zadanie\n"resolve + ID" oznacza zadanie jako wykonane'
-              f'\nhelp wyświetla pomoc\n')
-    elif user_command == 'exit':
+    elif command_check.lower() == REMOVE_TASK_COMMAND:
+        tasks = remove_task(split_user_command[1], tasks)
+    elif command_check.lower() == RESOLVE_TASK_COMMAND:
+        tasks = resolve_task(split_user_command[1], tasks)
+    elif command_check.lower() == PRINT_HELP_COMMAND:
+        print(f'"add + treść zadania" dodaje zadanie\n"remove + ID" usuwa zadanie\n"resolve + ID" oznacza zadanie jako'
+              f' wykonane\nhelp wyświetla pomoc\n')
+    elif user_command == EXIT_COMMAND:
         pass
     else:
         print("Niepoprawna komenda użytkownika\n")
@@ -76,7 +93,7 @@ def checking_user_input(tasks, user_command):
 
 
 def save_task(tasks):
-    with open(JSON_NAME, 'w') as file:
+    with open(JSON_FILE_NAME, 'w') as file:
         json.dump(tasks, file)
 
 
