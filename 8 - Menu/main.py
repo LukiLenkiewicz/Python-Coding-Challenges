11f1f3ad8fc37c8163a9329f34e7fae37207237f
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-08-11 07:51:43 +0200

    Update main.py

--- 8 - Menu/main.py
+++ 8 - Menu/main.py
@@ -4,36 +4,36 @@ EXIT = 'exit'
 ESC = 'esc'
 
 
-def menu():
-    user_command = ''
-    while user_command != EXIT:
-        user_command = input('"login" otwiera menu logowania\n"help" wyświetla pomoc\n"exit"opuszcza program\n')
-        if user_command == LOGIN:
-            login()
-        elif user_command == HELP:
-            help_interface()
-        elif user_command == EXIT:
-            pass
-        else:
-            print("Niepoprawna komenda użytkownika")
+class Menu:
+    def menu(self):
+        user_command = ''
+        while user_command != EXIT:
+            user_command = input(f'"{LOGIN}" otwiera menu logowania\n"{HELP}" wyświetla pomoc\n"{EXIT}" opuszcza program\n')
+            if user_command == LOGIN:
+                self.login()
+            elif user_command == HELP:
+                self.help_interface()
+            elif user_command == EXIT:
+                pass
+            else:
+                print("Niepoprawna komenda użytkownika")
+
+    def login(self):
+        user_name = input('Podaj dane logowania lub wpisz "esc aby wyjść: ')
+        if user_name != ESC:
+            print(f"Witaj {user_name}")
+        self.menu()
+
+    def help_interface(self):
+        print("Et Joseph dixit ad eum: Inserere haeret in asinum et adolebit. Dominici dixit: Insanis! Melius aditus ad"
+              " calida aqua. Et Joseph dixit ad eum: Elegantia Gallia. Ex Epistola ad Corinthos: submota olivarum! Amen")
+        while True:
+            user_command = input('wpisz "esc" aby wyjść ')
+            if user_command == ESC:
+                self.menu()
+            else:
+                print('Niepoprawna komenda użytkownika.')
 
 
-def login():
-    user_name = input('Podaj dane logowania lub wpisz "esc aby wyjść: ')
-    if user_name != ESC:
-        print(f"Witaj {user_name}")
-    menu()
-
-
-def help_interface():
-    print("Et Joseph dixit ad eum: Inserere haeret in asinum et adolebit. Dominici dixit: Insanis! Melius aditus ad"
-          " calida aqua. Et Joseph dixit ad eum: Elegantia Gallia. Ex Epistola ad Corinthos: submota olivarum! Amen")
-    while True:
-        user_command = input('wpisz "esc" aby wyjść ')
-        if user_command == ESC:
-            menu()
-        else:
-            print('Niepoprawna komenda użytkownika.')
-
-
-menu()
+obj = Menu()
+obj.menu()
