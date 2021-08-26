8cba4b6edb1d6dc66847dc5e48f101ace8ca5394
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-08-26 08:31:08 +0200

    Update main.py

--- 9 - Diffie-Hellman algorithm/main.py
+++ 9 - Diffie-Hellman algorithm/main.py
@@ -4,29 +4,43 @@ from math import sqrt
 class Diffie:
     def __init__(self):
         self.prime_number = 0
+        self.user1_number = ''
+        self.user2_number = ''
+        self.base = ''
 
     def interface(self):
-        base = ''
-        while not base.isdigit():
-            base = input("Podaj podstawę: ")
-        base = int(base)
+        self.input_data()
+        self.input_user_data()
+        key = self.calculate_secret_key()
+        print(f"Wygenerowany sekretny klucz: {key}")
+
+    def input_user_data(self):
+        while not self.user1_number.isdigit():
+            self.user1_number = input("Podaj liczbę pierwszego użytkownika: ")
+        self.user1_number = int(self.user1_number)
+        while not self.user2_number.isdigit():
+            self.user2_number = input("Podaj liczbę drugiego użytkownika: ")
+        self.user2_number = int(self.user2_number)
+    
+    def input_data(self):
+        while not self.base.isdigit():
+            self.base = input("Podaj podstawę: ")
+        self.base = int(self.base)
         while True:
-            self.prime_number = input("Podaj liczbę pierwszą: ")
+            self.prime_number = input("Podaj liczbę pierwszą")
             if self.prime_number.isdigit():
                 self.prime_number = int(self.prime_number)
                 if self.is_prime_number():
                     break
-        user1_number = ''
-        user2_number = ''
-        while not user1_number.isdigit():
-            user1_number = input("Podaj liczbę pierwszego użytkownika: ")
-        while not user2_number.isdigit():
-            user2_number = input("Podaj liczbę drugiego użytkownika: ")
-        user1_number = int(user1_number)
-        user2_number = int(user2_number)
-        a = pow(base, user1_number) % self.prime_number
-        key = pow(a, user2_number) % self.prime_number
-        print(f"Wygenerowany sekretny klucz: {key}")
+                else:
+                    print("Podana liczba musi być liczbą pierwszą: ")
+            else:
+                print("Musisz podać liczbę całkowitą")
+    
+    def calculate_secret_key(self):
+        a = pow(self.base, self.user1_number) % self.prime_number
+        key = pow(a, self.user2_number) % self.prime_number
+        return key
 
     def is_prime_number(self):
         for i in range(2, int(sqrt(self.prime_number))+1):
