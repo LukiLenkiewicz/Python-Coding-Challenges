4fc4a7ec307965ce69eba8347bea0883c0d6f69b
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-09-05 11:45:01 +0200

    Add files via upload

---
+++ 10 - Password Vault/commands.py
@@ -0,0 +1,10 @@
+class Commands:
+    def __init__(self):
+        self.ADD_WEBSITE = "add"
+        self.GENERATE_PASSWORD = "generate"
+        self.GET_PASSWORD = "get"
+        self.GET_ACCOUNTS = "accounts"
+        self.SAVE_PASSWORD = "save"
+        self.REGENERATE_PASSWORD = "regenerate"
+        self.DELETE_PASSWORD = "delete"
+        self.GET_HELP = "help"
