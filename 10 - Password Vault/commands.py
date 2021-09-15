3255b61a1431dc73c5b072512f97c9997d7629ca
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-09-15 11:09:15 +0200

    Update commands.py

--- 10 - Password Vault/commands.py
+++ 10 - Password Vault/commands.py
@@ -9,3 +9,11 @@ class Commands:
         self.DELETE_PASSWORD = "delete"
         self.GET_HELP = "help"
         self.USER_EXIT_COMMAND = "exit"
+        self.HELP_COMMAND_CONTENT = f'"{self.ADD_WEBSITE}" <nazwa strony> dodaje nową stronę\n"{self.GENERATE_PASSWORD}"' \
+                                    f'"{self.GENERATE_PASSWORD}" <nazwa strony> generuje hasło dla danej strony\n' \
+                                    f'"{self.GET_PASSWORD}" <nazwa strony> zwraca hasło dla konkretnej strony\n' \
+                                    f'"{self.GET_ACCOUNTS}" zwraca nazwy wszystkich stron dla których zostało ' \
+                                    f'zapisane hasło\n"{self.SAVE_PASSWORD}" <nazwa strony> zapisuje manualnie stworzone ' \
+                                    f'hasło dla konktretnej strony\n"{self.REGENERATE_PASSWORD}" tworzy nowe hasło\n' \
+                                    f'"{self.DELETE_PASSWORD}" <nazwa strony> usuwa konto'\
+                                    f'"{self.USER_EXIT_COMMAND}" opuszcza program'
