e882dd3d0e8b952098e7f4117fde99ad754a938a
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-09-15 12:07:22 +0200

    Update commands.py

--- 10 - Password Vault/commands.py
+++ 10 - Password Vault/commands.py
@@ -1,6 +1,5 @@
 class Commands:
     def __init__(self):
-        self.ADD_WEBSITE = "add"
         self.GENERATE_PASSWORD = "generate"
         self.GET_PASSWORD = "get"
         self.GET_ACCOUNTS = "accounts"
@@ -9,11 +8,12 @@ class Commands:
         self.DELETE_PASSWORD = "delete"
         self.GET_HELP = "help"
         self.USER_EXIT_COMMAND = "exit"
-        self.HELP_COMMAND_CONTENT = f'"{self.ADD_WEBSITE}" <nazwa strony> dodaje nową stronę\n"{self.GENERATE_PASSWORD}"' \
-                                    f'"{self.GENERATE_PASSWORD}" <nazwa strony> generuje hasło dla danej strony\n' \
+        self.HELP_COMMAND_CONTENT = f'"{self.GENERATE_PASSWORD}" <nazwa strony> dodaje nową stronę i generuje dla' \
+                                    f'niej haslo\n"{self.GENERATE_PASSWORD}" tworzy generuje nowe haslo\n' \
+                                    f'"{self.GENERATE_PASSWORD}" tworzy generuje nowe haslo\n' \
                                     f'"{self.GET_PASSWORD}" <nazwa strony> zwraca hasło dla konkretnej strony\n' \
                                     f'"{self.GET_ACCOUNTS}" zwraca nazwy wszystkich stron dla których zostało ' \
-                                    f'zapisane hasło\n"{self.SAVE_PASSWORD}" <nazwa strony> zapisuje manualnie stworzone ' \
-                                    f'hasło dla konktretnej strony\n"{self.REGENERATE_PASSWORD}" tworzy nowe hasło\n' \
-                                    f'"{self.DELETE_PASSWORD}" <nazwa strony> usuwa konto'\
+                                    f'zapisane hasło\n"{self.SAVE_PASSWORD}" <nazwa strony> zapisuje manualnie' \
+                                    f' stworzone hasło dla konktretnej strony\n"{self.REGENERATE_PASSWORD}"' \
+                                    f'tworzy nowe hasło\n"{self.DELETE_PASSWORD}" <nazwa strony> usuwa konto' \
                                     f'"{self.USER_EXIT_COMMAND}" opuszcza program'
