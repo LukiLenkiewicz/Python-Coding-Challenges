911c628ab1d98e3b2600d6eb183640b9e580409d
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-09-16 11:03:19 +0200

    Update handling_files.py

--- 10 - Password Vault/handling_files.py
+++ 10 - Password Vault/handling_files.py
@@ -1,31 +1,51 @@
-import json
+import sqlite3
 from cryptography.fernet import Fernet
 
-JSON_FILE_NAME = "hidden_passwords.json"
+DATABASE_NAME = "passwords.db"
+
+conn = sqlite3.connect(DATABASE_NAME)
+c = conn.cursor()
 
 
 def create_new_file():
     try:
-        with open(JSON_FILE_NAME, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        print("Tworzenie pustej listy haseł")
-        passwords = {}
-        with open(JSON_FILE_NAME, 'w') as file:
-            json.dump(passwords, file)
-        return passwords
+        c.execute("SELECT * FROM passwords")
+    except sqlite3.OperationalError:
+        print("Tworzenie nowej tabeli")
+        c.execute("""CREATE TABLE passwords (
+                website_name text,
+                website_password text
+            )""")
+        return {}
+    else:
+        data = c.fetchall()
+        data = turn_into_dictionary(data)
+        return data
 
 
-def save_task(passwords):
-    with open(JSON_FILE_NAME, 'w') as file:
-        json.dump(passwords, file)
+def turn_into_dictionary(data):
+    dic = {}
+    for password in data:
+        dic[password[0]] = password[1]
+    return dic
+
+
+def save_passwords(passwords):
+    passwords = passwords.items()
+    c.execute("DELETE FROM passwords")
+    c.executemany("INSERT INTO passwords VALUES (?, ?)", passwords)
+    conn.commit()
 
 
 def get_key():
-    KEY_FILE_NAME = "key.txt"
+    ACCESS_PASSWORD_FILE_NAME = "keys.db"
+    conn = sqlite3.connect(ACCESS_PASSWORD_FILE_NAME)
+    cur = conn.cursor()
     try:
-        with open(KEY_FILE_NAME, "r") as file_object:
-            key = file_object.read()
+        cur.execute("SELECT * FROM password_key_table")
+        key = cur.fetchall()
+        key = key[0][0]
+        conn.close()
     except FileNotFoundError:
         print("Nie znaleziono klucza")
     else:
