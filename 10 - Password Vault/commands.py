a0a32317b879201dd0cb62e35890695e3ecc588a
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-09-30 11:24:02 +0200

    Update commands.py

--- 10 - Password Vault/commands.py
+++ 10 - Password Vault/commands.py
@@ -1,19 +1,16 @@
-class Commands:
-    def __init__(self):
-        self.GENERATE_PASSWORD = "generate"
-        self.GET_PASSWORD = "get"
-        self.GET_ACCOUNTS = "accounts"
-        self.SAVE_PASSWORD = "save"
-        self.REGENERATE_PASSWORD = "regenerate"
-        self.DELETE_PASSWORD = "delete"
-        self.GET_HELP = "help"
-        self.USER_EXIT_COMMAND = "exit"
-        self.HELP_COMMAND_CONTENT = f'"{self.GENERATE_PASSWORD}" <nazwa strony> dodaje nową stronę i generuje dla' \
-                                    f'niej haslo\n"{self.GENERATE_PASSWORD}" tworzy generuje nowe haslo\n' \
-                                    f'"{self.GENERATE_PASSWORD}" tworzy generuje nowe haslo\n' \
-                                    f'"{self.GET_PASSWORD}" <nazwa strony> zwraca hasło dla konkretnej strony\n' \
-                                    f'"{self.GET_ACCOUNTS}" zwraca nazwy wszystkich stron dla których zostało ' \
-                                    f'zapisane hasło\n"{self.SAVE_PASSWORD}" <nazwa strony> zapisuje manualnie' \
-                                    f' stworzone hasło dla konktretnej strony\n"{self.REGENERATE_PASSWORD}"' \
-                                    f'tworzy nowe hasło\n"{self.DELETE_PASSWORD}" <nazwa strony> usuwa konto' \
-                                    f'"{self.USER_EXIT_COMMAND}" opuszcza program'
+GENERATE_PASSWORD = "generate"
+GET_PASSWORD = "get"
+GET_ACCOUNTS = "accounts"
+SAVE_PASSWORD = "save"
+REGENERATE_PASSWORD = "regenerate"
+DELETE_PASSWORD = "delete"
+GET_HELP = "help"
+USER_EXIT_COMMAND = "exit"
+HELP_COMMAND_CONTENT = f'"{GENERATE_PASSWORD}" <nazwa strony> dodaje nową stronę i generuje dla' \
+                       f'niej haslo\n"{GENERATE_PASSWORD}" generuje nowe haslo\n' \
+                       f'"{GET_PASSWORD}" <nazwa strony> zwraca hasło dla konkretnej strony\n' \
+                       f'"{GET_ACCOUNTS}" zwraca nazwy wszystkich stron dla których zostało ' \
+                       f'zapisane hasło\n"{SAVE_PASSWORD}" <nazwa strony> zapisuje manualnie' \
+                       f' stworzone hasło dla konktretnej strony\n"{REGENERATE_PASSWORD}" ' \
+                       f'<nazwa strony> tworzy nowe hasło\n"{DELETE_PASSWORD}" ' \
+                       f'<nazwa strony> usuwa konto\n"{USER_EXIT_COMMAND}" opuszcza program'
