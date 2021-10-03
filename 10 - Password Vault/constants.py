599ac478d1e25931a0df0007f51cc78e5715a814
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-10-03 20:17:27 +0200

    Update constants.py

--- 10 - Password Vault/constants.py
+++ 10 - Password Vault/constants.py
@@ -1,5 +1,8 @@
 PASSWORDS_DATABASE = "passwords.db"
 KEY_ACCESS_DATABASE = "keys.db"
 
+PASSWORD_KEY_TABLE_NAME = "password_key_table"
+ACCESS_KEY_TABLE_NAME = "access_key_table"
+
 MIN_PASSWORD_LENGTH = 8
 MAX_PASSWORD_LENGTH = 40
