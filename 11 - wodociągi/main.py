e36553f3a93787fa2c4fa69ccb90be115ef9d74b
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-10-03 15:19:32 +0200

    Update main.py

--- 11 - wodociągi/main.py
+++ 11 - wodociągi/main.py
@@ -1,41 +1,48 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-def wodociagi():
-    data = pd.read_csv("wodociagi.txt", sep=';')
 
+def wodociagi():
+    df = pd.read_csv("wodociagi.txt", sep=';')
+    format_data(df)
+    water_use_in_month(df.copy(deep=True))
+    monthly_use = table_of_months(df)
+    no_of_people = df["Liczba Mieszkańców"].sum()
+    water_use_per_inhabitant(monthly_use.copy(deep=True), no_of_people)
+    pie_chart(monthly_use.copy(deep=True))
+    plt.show()
+
+
+def format_data(data):
     districts = []
     no_of_people = []
-
     for id_ in data["KodKlienta"]:
         districts.append(id_[-3:])
         no_of_people.append(id_[-5:-3])
-
     no_of_people = list(map(int, no_of_people))
-    data.insert(0, "LiczbaMieszkańców", no_of_people)
-    data.insert(0, "Dzielnica", districts)
-    # data["Dzielnica"] = districts
-    # data["LiczbaMieszkańców"] = no_of_people
-    print(data)
-    df = pd.DataFrame(data.loc[:, "I":"LiczbaMieszkańców"])
-    df = df.groupby('Dzielnica').mean().reset_index()
+    data.insert(1, "Liczba Mieszkańców", no_of_people)
+    data.insert(1, "Dzielnica", districts)
+    data.pop("KodKlienta")
 
+
+def table_of_months(df):
+    return pd.DataFrame(df.loc[:, "I":"XII"]).sum().reset_index().rename(columns={'index': "Miesiąc", 0: "Zużycie wody"})
+
+
+def water_use_in_month(df):
+    df = df.groupby('Dzielnica').mean().reset_index()
     df.set_index("Dzielnica").T.plot(kind='bar', stacked=False, title="Średnie zużycie wody w danej dzielnicy w ciągu miesiąca.")
 
-    df = pd.DataFrame(data.loc[:, "I":"XII"])
-    new_plot = df.sum().reset_index()
-    new_plot = new_plot.rename(columns={'index': "Miesiąc", 0: "Liczba mieszkańców"})
-    plotpie = new_plot
-    new_plot["Liczba mieszkańców"] = new_plot["Liczba mieszkańców"]/sum(no_of_people)
 
-    new_plot.plot(x="Miesiąc", kind='bar', stacked=False, title="Średnie zużycie na mieszkańca", legend=None)
-    plt.show()
+def water_use_per_inhabitant(df, no_of_people):
+    df["Zużycie wody"] = df["Zużycie wody"]/no_of_people
+    df.plot(x="Miesiąc", kind='bar', stacked=False, title="Średnie zużycie na mieszkańca", legend=None)
 
-    print(plotpie)
+
+def pie_chart(df):
     label = ["" for _ in range(12)]
-    plotpie.plot(y="Liczba mieszkańców", autopct="%.2f", labels=label, kind='pie')
-    plt.legend(plotpie["Miesiąc"])
-    plt.show()
+    df.plot(y="Zużycie wody", autopct="%.2f", labels=label, kind='pie')
+    plt.legend(df["Miesiąc"])
+
 
 wodociagi()
