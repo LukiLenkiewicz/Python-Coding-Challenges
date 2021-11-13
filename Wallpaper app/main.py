05fc8952af2b5956969914e3360b21e81caa7874
LukiLenkiewicz <87516463+LukiLenkiewicz@users.noreply.github.com>
2021-11-13 15:09:01 +0100

    Update main.py

--- Wallpaper app/main.py
+++ Wallpaper app/main.py
@@ -6,10 +6,10 @@ import time
 import os
 
 
-
-
-def name():
-    pass
+def main(url):
+    response = get_response(url)
+    pic_urls = get_urls(response)
+    change_wallpaper(pic_urls)
 
 
 def get_response(url):
@@ -19,15 +19,23 @@ def get_response(url):
     return r.text
 
 
-def prepare_urls(matches):
-    return list({match.replace("\\u0026", "&") for match in matches})
+def get_urls(response):
+    pic_matches = re.findall('"display_url":"([^"]+)"', response)
+    return list({match.replace("\\u0026", "&") for match in pic_matches})
 
 
 def image_handler(image_url):
-    print(image_url)
     img_data = requests.get(image_url).content
     with open('wallpaper.jpg', 'wb') as handler:
         handler.write(img_data)
+
+
+def change_wallpaper(pic_urls):
+    WALLPAPER_PATH = get_wallpaper_path()
+    while True:
+        image_handler(random.choice(pic_urls))
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_PATH , 0)
+        time.sleep(3)
 
 
 def get_wallpaper_path():
@@ -36,15 +44,4 @@ def get_wallpaper_path():
     path += "/wallpaper.jpg"
     return path
 
-url = "https://www.instagram.com/lx_offiziell/"
-response = get_response(url)
-
-pic_matches = re.findall('"display_url":"([^"]+)"', response)
-pic_urls = prepare_urls(pic_matches)
-
-WALLPAPER_PATH = get_wallpaper_path()
-
-while True:
-    image_handler(random.choice(pic_urls))
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_PATH , 0)
-    time.sleep(3)
+main("https://www.instagram.com/lx_offiziell/")
